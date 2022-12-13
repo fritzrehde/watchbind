c8f6ff06c48961a3ce36f510927d81fe24933199
Fritz Rehde <fritzrehde.work@gmail.com>
2022-12-13 17:29:35 +0100

    Added selected property to each line

--- src/stateful_list.rs
+++ src/stateful_list.rs
@@ -1,16 +1,51 @@
-use std::cmp::min;
 use crate::style::Styles;
+use itertools::izip;
+use std::cmp::min;
 use tui::{
 	backend::Backend,
-	widgets::{Cell, Row, Table, TableState},
 	layout::Constraint,
+	style::{Color, Style},
+	widgets::{Cell, Row, Table, TableState},
 	Frame,
 };
 
 const FIRST_INDEX: usize = 0;
 
+// TODO: replace vectors with slices
+
+pub struct Line {
+	line: String,
+	selected: bool,
+}
+
+impl Line {
+	fn new(line: &str, selected: bool) -> Line {
+		Line {
+			// TODO consider to_owned()
+			line: line.to_string(),
+			selected,
+		}
+	}
+
+	fn format(&mut self) -> Row {
+		Row::new(vec![
+			Cell::from(" ").style(Style::reset().bg(Color::Magenta)),
+			Cell::from(self.line.clone()),
+		])
+	}
+
+	fn cells(&mut self) -> Vec<Cell> {
+		vec![
+			Cell::from(" ").style(Style::reset().bg(Color::Magenta)),
+			Cell::from(self.line.clone()),
+		]
+	}
+}
+
 pub struct StatefulList {
 	lines: Vec<String>,
+	// TODO: rename to selected_lines
+	selected: Vec<bool>,
 	state: TableState,
 	styles: Styles,
 }
@@ -18,6 +53,8 @@ pub struct StatefulList {
 impl StatefulList {
 	pub fn new(lines: Vec<String>, styles: &Styles) -> StatefulList {
 		StatefulList {
+			// TODO: construct vec of length lines.len() containing only false
+			selected: (0..lines.len()).map(|_| false).collect(),
 			lines,
 			state: TableState::default(),
 			styles: *styles,
@@ -26,24 +63,37 @@ impl StatefulList {
 
 	pub fn draw<B: Backend>(&mut self, frame: &mut Frame<B>) {
 		// TODO: remove clone()
-		let rows: Vec<Row> = self.lines
-			.iter()
-			// .map(|line| Row::new(vec![Cell::from(" "), Cell::from(line.clone())]))
-			.map(|line| Row::new(vec![Cell::from(line.clone())]))
+		let rows: Vec<Row> = izip!(self.lines.iter(), self.selected.iter())
+			// .map(|(line, selected)| Row::new(vec![line.clone()]))
+			// .map(|(line, selected)| Row::new(Line::new(line, *selected).cells().to_owned()))
+			.map(|(line, &selected)| {
+				Row::new(vec![
+					Cell::from(" ").style(if selected {
+						// TODO: allow customize color
+						Style::reset().bg(Color::Magenta)
+					} else {
+						Style::reset()
+					}),
+					Cell::from(line.clone()),
+				])
+				// Row::new(Line::new(line, *selected).cells().to_owned())
+			})
+			// .map(|(line, selected)| Line::new(line, *selected).format())
 			.collect();
 
 		// TODO: still very hacky
 		let table = Table::new(rows)
 			.style(self.styles.style)
 			.highlight_style(self.styles.highlight_style)
-			.widths(&[Constraint::Percentage(100)])
-			.column_spacing(0);
+			// .widths(&[Constraint::Percentage(100)])
+			.widths(&[Constraint::Length(1), Constraint::Percentage(100)])
+			.column_spacing(1);
 
 		frame.render_stateful_widget(table, frame.size(), &mut self.state);
 	}
 
 	pub fn set_lines(&mut self, lines: Vec<String>) {
-		self.lines = lines.clone();
+		self.lines = lines;
 		// TODO: optimize through earlier if statements
 		self.calibrate_selected_line();
 	}
